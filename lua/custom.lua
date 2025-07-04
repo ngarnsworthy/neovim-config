266696e7fd8ad866b0b77da78b385c0941c127a8
Nick Garnsworthy <ngarnsworthy@gmail.com>
2025-07-04 09:25:22 -0500

    Ad custom.lua

---
+++ lua/custom.lua
@@ -0,0 +1,17 @@
+local M = {}
+
+M.plugins = {
+  { "Shatur/neovim-ayu" },
+  {
+    "m4xshen/hardtime.nvim",
+    lazy = false,
+    dependencies = { "MunifTanjim/nui.nvim" },
+    opts = { max_count = 1, max_time = 5000 },
+  },
+  { "sitiom/nvim-numbertoggle" },
+}
+
+M.configs = function() require("ayu").colorscheme() end
+
+
+return M
