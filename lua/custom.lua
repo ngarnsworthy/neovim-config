61ae69ff9cea24e9dc401da357d4ece2f7f068d0
Nicholas Garnsworthy <ngarnsworthy@gmail.com>
2025-11-10 13:34:36 -0600

    Add wakatime

--- lua/custom.lua
+++ lua/custom.lua
@@ -1,17 +1,17 @@
 local M = {}
 
 M.plugins = {
-  { "Shatur/neovim-ayu" },
-  {
-    "m4xshen/hardtime.nvim",
-    lazy = false,
-    dependencies = { "MunifTanjim/nui.nvim" },
-    opts = { max_count = 1, max_time = 5000 },
-  },
-  { "sitiom/nvim-numbertoggle" },
+    { "Shatur/neovim-ayu" },
+    {
+        "m4xshen/hardtime.nvim",
+        lazy = false,
+        dependencies = { "MunifTanjim/nui.nvim" },
+        opts = { max_count = 1, max_time = 5000 },
+    },
+    { "sitiom/nvim-numbertoggle" },
+    { "wakatime/vim-wakatime" },
 }
 
 M.configs = function() require("ayu").colorscheme() end
-
 
 return M
