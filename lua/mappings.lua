2e8deb043b04638a9870a1e76f60fcce2f176f9c
Nicholas Garnsworthy <ngarnsworthy@gmail.com>
2025-12-01 10:19:12 -0600

    Add LSP bindings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -36,11 +36,11 @@ map("n", "<leader>wl", "<C-w>l", { desc = "switch window down" })
 -- 	vim.notify("Nvim configuration reloaded!", vim.log.levels.INFO)
 -- end
 function _G.reload_config()
-  local reload = require("plenary.reload").reload_module
-  reload("me", false)
+    local reload = require("plenary.reload").reload_module
+    reload("me", false)
 
-  dofile(vim.env.MYVIMRC)
-  vim.notify("Nvim configuration reloaded!", vim.log.levels.INFO)
+    dofile(vim.env.MYVIMRC)
+    vim.notify("Nvim configuration reloaded!", vim.log.levels.INFO)
 end
 
 map("n", "rr", _G.reload_config, { desc = "Reload configuration without restart nvim" })
@@ -54,17 +54,23 @@ map("n", "<leader>fh", builtin.help_tags, { desc = "Open Telescope to show help"
 map("n", "<leader>fo", builtin.oldfiles, { desc = "Open Telescope to list recent files" })
 map("n", "<leader>cm", builtin.git_commits, { desc = "Open Telescope to list git commits" })
 -- NvimTree
-map("n", "<leader>n", ":NvimTreeToggle<CR>", { desc = "Toggle NvimTree sidebar" })    -- open/close
-map("n", "<leader>nr", ":NvimTreeRefresh<CR>", { desc = "Refresh NvimTree" })         -- refresh
+map("n", "<leader>n", ":NvimTreeToggle<CR>", { desc = "Toggle NvimTree sidebar" }) -- open/close
+map("n", "<leader>nr", ":NvimTreeRefresh<CR>", { desc = "Refresh NvimTree" }) -- refresh
 map("n", "<leader>nf", ":NvimTreeFindFile<CR>", { desc = "Search file in NvimTree" }) -- search file
 
 -- LSP
 map(
-  "n",
-  "<leader>gm",
-  function() require("conform").format { lsp_fallback = true } end,
-  { desc = "General Format file" }
+    "n",
+    "<leader>gm",
+    function() require("conform").format { lsp_fallback = true } end,
+    { desc = "General Format file" }
 )
+map("n", "<leader>gf", function()
+    vim.diagnostic.goto_next()
+    vim.lsp.buf.code_action()
+end, { desc = "Goto and fix next error" })
+map("n", "<leader>gs", function() vim.lsp.buf.code_action() end, { desc = "Shows code actions" })
+map("n", "<leader>gn", function() vim.diagnostic.goto_next() end, { desc = "Goes to next code error" })
 
 -- global lsp mappings
 map("n", "<leader>ds", vim.diagnostic.setloclist, { desc = "LSP Diagnostic loclist" })
@@ -75,28 +81,28 @@ map("v", "mm", "gc", { desc = "Toggle comment", remap = true })
 
 -- Terminal
 map("n", "tt", function()
-  local term_width = math.floor(vim.o.columns * 0.8)
-  local term_height = math.floor(vim.o.lines * 0.7);
-  local row = math.floor((vim.o.lines - term_height) / 2);
-  local col = math.floor((vim.o.columns - term_width) / 2);
+    local term_width = math.floor(vim.o.columns * 0.8)
+    local term_height = math.floor(vim.o.lines * 0.7)
+    local row = math.floor((vim.o.lines - term_height) / 2)
+    local col = math.floor((vim.o.columns - term_width) / 2)
 
-  vim.api.nvim_open_win(0, true, {
-    relative = 'editor',
-    row = row,
-    col = col,
-    width = term_width,
-    height = term_height,
-    border = 'rounded',
-    style = 'minimal',
-  })
+    vim.api.nvim_open_win(0, true, {
+        relative = "editor",
+        row = row,
+        col = col,
+        width = term_width,
+        height = term_height,
+        border = "rounded",
+        style = "minimal",
+    })
 
-  vim.api.nvim_win_set_buf(0, vim.api.nvim_create_buf(false, true))
-  vim.cmd('startinsert')
-  vim.cmd('terminal')
+    vim.api.nvim_win_set_buf(0, vim.api.nvim_create_buf(false, true))
+    vim.cmd "startinsert"
+    vim.cmd "terminal"
 
-  -- Close terminal on exit
-  vim.api.nvim_buf_set_keymap(0, 't', '<esc>', '<CR>cd<CR>exit<CR>', { noremap = true, silent = true })
-  vim.api.nvim_buf_set_keymap(0, 't', 'jk', '<CR>cd<CR>exit<CR>', { noremap = true, silent = true })
+    -- Close terminal on exit
+    vim.api.nvim_buf_set_keymap(0, "t", "<esc>", "<CR>cd<CR>exit<CR>", { noremap = true, silent = true })
+    vim.api.nvim_buf_set_keymap(0, "t", "jk", "<CR>cd<CR>exit<CR>", { noremap = true, silent = true })
 end, { noremap = true, silent = true })
 
 map("n", "<Tab>", ":bn<CR>", { desc = "Next buffer" })
